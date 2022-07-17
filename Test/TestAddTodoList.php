ce1c9e57c15f06e629830ecf3da3f80ed0de2392
arifinlenna <arifin@lenna.ai>
2022-07-17 14:01:11 +0700

    add show//add todolist and test show//add todolit

---
+++ Test/TestAddTodoList.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '/Users/arifinlenna/Documents/belajar/PHP-Dasar-TodoList/BusinessLogic/AddTodoList.php';
+require_once '/Users/arifinlenna/Documents/belajar/PHP-Dasar-TodoList/Model/TodoList.php';
+
+addTodoList("arifin");
+print_r($todolist);
---
+++ Test/TestShowTodoList.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once '/Users/arifinlenna/Documents/belajar/PHP-Dasar-TodoList/BusinessLogic/ShowTodoList.php';
+require_once '/Users/arifinlenna/Documents/belajar/PHP-Dasar-TodoList/Model/TodoList.php';
+
+showTodoList();
